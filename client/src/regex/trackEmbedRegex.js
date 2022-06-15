c484302e6b3572a63c759b09126247ce99f9d5c5
Keo222 <kyote222@gmail.com>
2022-06-15 12:44:56 -0700

    regex and all styled-components to TS. Done!

--- client/src/regex/trackEmbedRegex.js
+++
@@ -1,6 +0,0 @@
-// Spotify Regex
-export const spotifyRegex = /\/[\d\w]+[?]/gm;
-// Tidal Regex
-export const tidalRegex = /\/[\d]+$/gm;
-// Apple Regex
-export const appleRegex = /\/[\d\w-%&]+\/[\d]+[?i=]{3}[\d]+/gm;
