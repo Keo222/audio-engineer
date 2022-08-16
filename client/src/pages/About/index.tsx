015d11c67d309a9df34456350b7f484715b35222
Keo222 <kyote222@gmail.com>
2022-08-16 09:51:15 -0700

    made contact logos section its own component

--- client/src/pages/About/index.tsx
+++ client/src/pages/About/index.tsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Helmet } from "react-helmet";
 
+import ContactLogos from "components/ContactLogos";
+
 // Images
 import aboutImgSrc from "images/about_filler.jpg";
 
-// Social Logos
-import { instagramLogo, facebookLogo, mailIcon } from "images/icons";
 // Imported Styled Components
 import { PageHeading, FormattedParagraph } from "styled/typography";
 
@@ -110,83 +110,6 @@ const AboutInfoSection = styled.section`
   }
 `;
 
-const InlineSocialsSection = styled.section`
-  width: 30rem;
-  height: 8rem;
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  @media screen and (${(props) => props.theme.responsive.sm}) {
-    width: 20rem;
-  }
-`;
-
-const IconDiv = styled.div`
-  position: relative;
-  &:hover > span {
-    visibility: visible;
-    opacity: 1;
-  }
-  cursor: pointer;
-`;
-
-const SocialIconDiv = styled(IconDiv)`
-  height: 2.7rem;
-  width: 2.7rem;
-`;
-
-const MailIconDiv = styled(IconDiv)`
-  height: 3rem;
-  width: 3rem;
-`;
-
-const Tooltip = styled.span`
-  visibility: hidden;
-  width: fit-content;
-  background-color: ${(props) => props.theme.color.textDark};
-  color: ${(props) => props.theme.color.textLight};
-  padding: 5px;
-  border: 1px solid #aaa;
-  border-radius: 5px;
-
-  /* Tooltip Text */
-  white-space: nowrap;
-  font-size: 1.2rem;
-
-  /* Position the tooltip text */
-  position: absolute;
-  z-index: 1;
-  bottom: 125%;
-  left: 30%;
-
-  /* Fade in tooltip */
-  opacity: 0;
-  transition: opacity 0.3s;
-
-  &::after {
-    content: "";
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    margin-left: -5px;
-    border-width: 5px;
-    border-style: solid;
-    border-color: #eee transparent transparent transparent;
-  }
-`;
-
-const Icon = styled.img`
-  object-fit: contain;
-  width: 100%;
-  height: 100%;
-  transition: all 0.15s;
-  filter: grayscale(20%) brightness(0.8);
-  &:hover {
-    filter: grayscale(0) brightness(1.3);
-  }
-`;
-
 const About = () => {
   const [text, setText] = useState([]);
 
@@ -217,20 +140,7 @@ const About = () => {
             <FormattedParagraph key={i}>{p}</FormattedParagraph>
           ))}
         </AboutInfoSection>
-        <InlineSocialsSection aria-label="contact buttons">
-          <SocialIconDiv>
-            <Tooltip>Instagram</Tooltip>
-            <Icon src={instagramLogo} />
-          </SocialIconDiv>
-          <SocialIconDiv>
-            <Tooltip>Facebook</Tooltip>
-            <Icon src={facebookLogo} />
-          </SocialIconDiv>
-          <MailIconDiv>
-            <Tooltip>Email</Tooltip>
-            <Icon src={mailIcon} />
-          </MailIconDiv>
-        </InlineSocialsSection>
+        <ContactLogos />
       </AboutInfoContainer>
     </>
   );
