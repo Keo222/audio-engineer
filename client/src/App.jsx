6d2bfe0c1aa86c6d0ab84fbf5b7875c497b3052e
Keo222 <kyote222@gmail.com>
2022-08-15 18:06:43 -0700

    moved About page into it's own folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,29 @@
-import React from "react";
 // REACT-ROUTER-DOM
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // STYLED COMPONENTS
 import { ThemeProvider } from "styled-components";
 // Theme Style
-import { theme } from "./styled/themes";
-import GlobalStyle from "./styled/GlobalStyle";
+import { theme } from "styled/themes";
+import GlobalStyle from "styled/GlobalStyle";
 
 // PAGES
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Listen from "./pages/Listen";
-import Contact from "./pages/Contact";
-import Services from "./pages/Services";
-import Hire from "./pages/Hire";
-import Admin from "./pages/admin/AdminHome";
-import AdminLogin from "./pages/admin/AdminLogin";
-import AdminTracks from "./pages/admin/AdminTracks";
-import AdminNewTrack from "./pages/admin/AdminNewTrack";
-import FormikTSAdminNewTrack from "./pages/admin/FormikTSAdminNewTrack";
-import AdminUpdateTrack from "./pages/admin/AdminUpdateTrack";
-import AdminText from "./pages/admin/AdminText";
-import AdminGenres from "./pages/admin/AdminGenres";
+import Home from "pages/Home";
+import About from "pages/About";
+import Listen from "pages/Listen";
+import Contact from "pages/Contact";
+import Services from "pages/Services";
+import Hire from "pages/Hire";
+import Admin from "pages/admin/AdminHome";
+import AdminLogin from "pages/admin/AdminLogin";
+import AdminTracks from "pages/admin/AdminTracks";
+import AdminNewTrack from "pages/admin/AdminNewTrack";
+import FormikTSAdminNewTrack from "pages/admin/FormikTSAdminNewTrack";
+import AdminUpdateTrack from "pages/admin/AdminUpdateTrack";
+import AdminText from "pages/admin/AdminText";
+import AdminGenres from "pages/admin/AdminGenres";
 
 // Components
-import Navbar from "./components/Navbar";
+import Navbar from "components/Navbar";
 
 const App = () => {
   return (
