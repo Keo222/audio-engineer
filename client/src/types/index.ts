fd88e10d1b51a5cb3867794cf6a4474255c4572d
Keo222 <kyote222@gmail.com>
2022-08-15 17:52:05 -0700

    changed types file structure

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,10 +1,29 @@
-import type {
-  TGenre,
-  TPlayer,
-  TText,
-  TTextTitle,
-  TTrack,
-  TWork,
-} from "./types";
+type TGenre = { genre_name: string };
+
+type TPlayer = "Spotify" | "Tidal" | "Apple";
+
+type TText = {
+  name: string;
+  stored_text: string[];
+};
+
+type TTextTitle = "about" | "contact" | "pricing" | "hire";
+
+type TTrack = {
+  track_id: number;
+  track_genre: string;
+  track_work: string;
+  track_name: string;
+  track_year: number;
+  track_artist: string;
+  track_album: string;
+  track_featured: boolean;
+  track_spotify: string;
+  track_tidal: string;
+  track_apple: string;
+  numOrder?: number;
+};
+
+type TWork = "Production" | "Mixing" | "Mastering" | "All";
 
 export type { TGenre, TPlayer, TText, TTextTitle, TWork, TTrack };
