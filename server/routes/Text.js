e0c3e013add07380a740a5a32323e1826b0d2ed4
Kyle Olsen <kyle.olsen222@gmail.com>
2022-10-11 15:48:41 -0700

    fix autocomplete errors

--- server/routes/Text.js
+++ server/routes/Text.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("..db/db");
+const pool = require("../db");
 
 // GET TEXT
 router.get("/", async (req, res) => {
