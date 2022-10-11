3fbb8b0e3f5b58cb2d68d998e48e2ecda6ebd755
Kyle Olsen <kyle.olsen222@gmail.com>
2022-10-11 14:46:32 -0700

    Contact form now using Formik

---
+++ client/src/pages/Contact/pageComponents/FormikContactForm.tsx
@@ -0,0 +1,130 @@
+import styled from "styled-components";
+import { Formik, useFormik } from "formik";
+import * as Yup from "yup";
+
+// Styled Components
+import {
+  StyledForm,
+  InputGroup,
+  InputLabel,
+  TextInput,
+  SubmitButton,
+} from "styled/forms";
+
+const StyledTextArea = styled.textarea`
+  height: 15rem;
+  width: 100%;
+`;
+
+const ErrorMessage = styled.p`
+  margin: 0;
+  font-weight: 600;
+  font-size: 14px;
+  color: ${(props) => props.theme.color.highlight3};
+`;
+
+const FormikContactForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Name required"),
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email required"),
+      subject: Yup.string()
+        .max(100, "Subject too long")
+        .required("Subject required"),
+      message: Yup.string().required("Message required"),
+    }),
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values));
+    },
+  });
+  return (
+    <Formik
+      initialValues={{ name: "", email: "", subject: "", message: "" }}
+      validationSchema={Yup.object({
+        name: Yup.string().required("Name required"),
+        email: Yup.string()
+          .email("Invalid email address")
+          .required("Email required"),
+        subject: Yup.string()
+          .max(100, "Subject too long")
+          .required("Subject required"),
+        message: Yup.string().required("Message required"),
+      })}
+      onSubmit={(values) => {
+        console.log(JSON.stringify(values));
+      }}
+    >
+      <StyledForm onSubmit={formik.handleSubmit} aria-label="Contact Form">
+        <InputGroup>
+          <InputLabel htmlFor="name">Name:</InputLabel>
+          <TextInput
+            type="text"
+            name="name"
+            id="name"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
+          />
+          {formik.touched.name && formik.errors.name && (
+            <ErrorMessage>{formik.errors.name}</ErrorMessage>
+          )}
+        </InputGroup>
+
+        <InputGroup>
+          <InputLabel htmlFor="email">Email:</InputLabel>
+          <TextInput
+            type="email"
+            name="email"
+            id="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email && (
+            <ErrorMessage>{formik.errors.email}</ErrorMessage>
+          )}
+        </InputGroup>
+
+        <InputGroup>
+          <InputLabel htmlFor="subject">Subject:</InputLabel>
+          <TextInput
+            type="text"
+            name="subject"
+            id="subject"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.subject}
+          />
+          {formik.touched.subject && formik.errors.subject && (
+            <ErrorMessage>{formik.errors.subject}</ErrorMessage>
+          )}
+        </InputGroup>
+
+        <InputGroup>
+          <InputLabel htmlFor="message">Message:</InputLabel>
+          <StyledTextArea
+            name="message"
+            id="message"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.message}
+          />
+          {formik.touched.message && formik.errors.message && (
+            <ErrorMessage>{formik.errors.message}</ErrorMessage>
+          )}
+        </InputGroup>
+        <SubmitButton type="submit">Submit</SubmitButton>
+      </StyledForm>
+    </Formik>
+  );
+};
+
+export default FormikContactForm;
