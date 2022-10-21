6abc72e14f783202b2ffc8927b805dfe1acbc356
Kyle Olsen <kyle.olsen222@gmail.com>
2022-10-20 19:08:10 -0700

    hire form select tags webkit styling

--- client/src/pages/Hire/styled/StyledHireForm.ts
+++ client/src/pages/Hire/styled/StyledHireForm.ts
@@ -64,6 +64,16 @@ export const StyledSelect = styled.select<TGrid & TColorProp>`
   grid-column-start: ${(props) => props.colStart};
   grid-column-end: ${(props) => props.colEnd};
 
+  /* Needed to make Safari <select> element to look the same */
+  appearance: none;
+  background-image: url("data:image/svg+xml;utf8,<svg fill='black' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
+  background-repeat: no-repeat;
+  background-position-x: 100%;
+  padding: 0 5px;
+  font-size: 14px;
+  border: none;
+  border-radius: 2px;
+
   &:focus,
   &:focus-visible {
     outline-color: ${(props) =>
