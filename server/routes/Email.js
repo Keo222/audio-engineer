f4013e08417a85c35f1b8d641ffb7c26787119e5
Kyle Olsen <kyle.olsen222@gmail.com>
2022-10-21 15:12:53 -0700

    contact and hire forms now work in dev

--- server/routes/Email.js
+++ server/routes/Email.js
@@ -1,48 +1,28 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
 const router = express.Router();
 require("dotenv").config({ path: __dirname + "/../.env" });
 
-const OAuth2Client = new google.auth.OAuth2(
-  process.env.CLIENT_ID,
-  process.env.OAUTH_CLIENT_SECRET,
-  process.env.REDIRECT_URI
-);
-
-OAuth2Client.setCredentials({
-  refresh_token: process.env.OAUTH_REFRESH_TOKEN,
-});
+const CLIENT_EMAIL = process.env.EMAIL_USERNAME;
+const CLIENT_PASS = process.env.EMAIL_PASS;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.OAUTH_CLIENT_SECRET;
+const REFRESH_TOKEN = process.env.OAUTH_REFRESH_TOKEN;
 
 router.post("/contact", async (req, res) => {
   const { name, email, subject, message } = req.body;
 
-  const CLIENT_EMAIL = process.env.EMAIL_USERNAME;
-  const CLIENT_ID = process.env.CLIENT_ID;
-  const CLIENT_SECRET = process.env.OAUTH_CLIENT_SECRET;
-  const REDIRECT_URI = process.env.REDIRECT_URI;
-  const REFRESH_TOKEN = process.env.OAUTH_REFRESH_TOKEN;
-  const OAuth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URI
-  );
-  OAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
   try {
-    // Generate the accessToken
-    const accessToken = await OAuth2Client.getAccessToken();
-
     // Nodemailer transporter
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
         type: "OAuth2",
         user: CLIENT_EMAIL,
+        pass: CLIENT_PASS,
         clientId: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
         refreshToken: REFRESH_TOKEN,
-        accessToken: accessToken,
       },
     });
 
@@ -66,32 +46,17 @@ router.post("/contact", async (req, res) => {
 router.post("/hire", async (req, res) => {
   const { name, work, numTracks, email, subject, message } = req.body;
 
-  const CLIENT_EMAIL = process.env.EMAIL_USERNAME;
-  const CLIENT_ID = process.env.CLIENT_ID;
-  const CLIENT_SECRET = process.env.OAUTH_CLIENT_SECRET;
-  const REDIRECT_URI = process.env.REDIRECT_URI;
-  const REFRESH_TOKEN = process.env.OAUTH_REFRESH_TOKEN;
-  const OAuth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URI
-  );
-  OAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
   try {
-    // Generate the accessToken
-    const accessToken = await OAuth2Client.getAccessToken();
-
     // Nodemailer transporter
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
         type: "OAuth2",
         user: CLIENT_EMAIL,
+        pass: CLIENT_PASS,
         clientId: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
         refreshToken: REFRESH_TOKEN,
-        accessToken: accessToken,
       },
     });
 
