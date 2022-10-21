b9b707efbe1755d029d64cd669e2408f80c62e20
Kyle Olsen <kyle.olsen222@gmail.com>
2022-10-21 16:04:34 -0700

    brought back accessToken logic and currently have both password and access token

--- server/routes/Email.js
+++ server/routes/Email.js
@@ -2,17 +2,31 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const router = express.Router();
 require("dotenv").config({ path: __dirname + "/../.env" });
+const { google } = require("googleapis");
 
 const CLIENT_EMAIL = process.env.EMAIL_USERNAME;
-const CLIENT_PASS = process.env.APP_PASS;
+const CLIENT_PASS = process.env.EMAIL_PASS;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.OAUTH_CLIENT_SECRET;
 const REFRESH_TOKEN = process.env.OAUTH_REFRESH_TOKEN;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+// OAUTH
+const OAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.OAUTH_CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+OAuth2Client.setCredentials({
+  refresh_token: process.env.OAUTH_REFRESH_TOKEN,
+});
 
 router.post("/contact", async (req, res) => {
   const { name, email, subject, message } = req.body;
 
   try {
+    const accessToken = await OAuth2Client.getAccessToken();
     // Nodemailer transporter
     const transporter = nodemailer.createTransport({
       service: "gmail",
@@ -23,6 +37,7 @@ router.post("/contact", async (req, res) => {
         clientId: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
         refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken,
       },
     });
 
@@ -47,6 +62,7 @@ router.post("/hire", async (req, res) => {
   const { name, work, numTracks, email, subject, message } = req.body;
 
   try {
+    const accessToken = await OAuth2Client.getAccessToken();
     // Nodemailer transporter
     const transporter = nodemailer.createTransport({
       service: "gmail",
@@ -57,6 +73,7 @@ router.post("/hire", async (req, res) => {
         clientId: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
         refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken,
       },
     });
 
